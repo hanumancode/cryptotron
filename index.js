07a75f691afacbc9962788005dbc8dbf89dee724
Gareth Jones <hanumancode@gmail.com>
2019-05-08 18:17:33 +0100

    cryptotron bot initial commmit with getProducts method for GDAX crypto data API

---
+++ index.js
@@ -0,0 +1,14 @@
+const GDAX = require('gdax');
+const publicClient = new GDAX.PublicClient();
+
+const callback = (error, response, data) => {
+    if (error)
+        return console.dir(error);
+    return console.dir(data);
+}
+
+//publicClient.getProducts(callback);
+
+publicClient.getCurrencies(callback);
+
+
